3a08528e70b331d689cfa91ca8e7caee5c7acfea
sunbee <sanjay.bhatikar@gmail.com>
2023-01-25 19:28:14 -0600

    Added feature login form redir auth usr to land pg

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from starlette.middleware.sessions import SessionMiddleware
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, HTMLResponse
 from pydantic import BaseModel
 import jwt
+import re
 
 class User(BaseModel):
     uid: str
@@ -12,17 +13,34 @@ class User(BaseModel):
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key="top_secret")
 
-@app.post('/login/')
-async def sign_in(uid: str, pwd: str, session: dict, request: Request):
-    request.session["jwt_token"] = uid+"+"+pwd
-    return 'ok'
+def issue_token(uid, pwd):
+    token = uid + "+" + pwd
+    return token
+
+def decode_token(toke):
+    return toke.split('+')
+
+@app.get('/login/')
+async def sign_in():
+    with open('signin.html', 'r') as fd:
+        signin_HTML = fd.read()
+    return HTMLResponse(signin_HTML)
+
+@app.post('/token/')
+async def tokenize(request: Request, uid: str = Form(...), pwd: str = Form(...)):
+    token = issue_token(uid, pwd)
+    request.session["jwt_token"] = token
+    return RedirectResponse(url='/', status_code=302)
 
 @app.get("/")
 async def land_me(request: Request):
     token = request.session.get('jwt_token', None)
     if token:
-        return {"Pass": token}
+        uid, pwd = decode_token(token)
+        return {'status': 'pass', 'uid': uid, 'pwd': pwd}
     else:
-        return {"Pass": "You shall not!"}
+        return {"status": "fail"}
     
-
+@app.get('/welcome')
+async def whoami():
+    pass
